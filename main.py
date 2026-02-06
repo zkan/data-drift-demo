2746d5c53f61b7ad5bb5bf8842cd1b932932f2b8
zkan <kan@odds.team>
2026-02-06 20:38:30 +0700

    Use Python 3.12.12 (compatitble with evidently)

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import pandas as pd
 from sklearn import datasets
 
-# from evidently import Dataset
-# from evidently import DataDefinition
-# from evidently import Report
-# from evidently.presets import DataDriftPreset, DataSummaryPreset
+from evidently import Dataset
+from evidently import DataDefinition
+from evidently import Report
+from evidently.presets import DataDriftPreset, DataSummaryPreset
 
 
 def main():
