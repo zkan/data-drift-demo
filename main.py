8eb59f40df21cf3ae4ff9f80a76a61128f3dd291
zkan <kan@odds.team>
2026-02-06 20:06:49 +0700

    Load adult dataset

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import pandas as pd
+from sklearn import datasets
+
+# from evidently import Dataset
+# from evidently import DataDefinition
+# from evidently import Report
+# from evidently.presets import DataDriftPreset, DataSummaryPreset
+
+
 def main():
     print("Hello from data-drift-demo!")
+
+    adult_data = datasets.fetch_openml(name="adult", version=2, as_frame="auto")
+    adult = adult_data.frame
+    print(adult)
 
 
 if __name__ == "__main__":
